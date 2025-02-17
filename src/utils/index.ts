133eb974dcd091387e159404758d05262cd3490f
Liubou Hadunova <liubou.hadunova@leverx.com>
2025-02-17 13:48:27 +0100

    fix: update eslint rules

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,18 +1,16 @@
 //type for currying function
-type CurryFunction<T extends any[], R> = T extends [infer A, ...infer Rest]
-  ? (arg: A) => CurryFunction<Rest, R>
-  : R;
+type CurryFunction<T extends any[], R> = T extends [infer A, ...infer Rest] ? (arg: A) => CurryFunction<Rest, R> : R;
 
 //currying function
 //example: sum(1,2) = sum(1)(2);
-  export const carrying = <T extends any[], R>(func: (...args: T) => R): CurryFunction<T, R> => {
-     return function curried(...args: any[]): any {
-         if (args.length >= func.length) {
-             return func(...args as T);
-         }
+export const carrying = <T extends any[], R>(func: (...args: T) => R): CurryFunction<T, R> => {
+    return function curried(...args: any[]): any {
+        if (args.length >= func.length) {
+            return func(...(args as T));
+        }
 
-         return function(...otherArgs: any[]) {
-             return curried(...([...args, ...otherArgs] as any[]));
-         }
-     } as CurryFunction<T, R>;
- }
+        return function (...otherArgs: any[]) {
+            return curried(...([...args, ...otherArgs] as any[]));
+        };
+    } as CurryFunction<T, R>;
+};
