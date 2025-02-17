a639df66d26d6a6342d8e447802271c2d1b25e0f
Liubou Hadunova <liubou.hadunova@leverx.com>
2025-02-17 13:40:57 +0100

    feat: add currying function

---
+++ src/utils/index.ts
@@ -0,0 +1,18 @@
+//type for currying function
+type CurryFunction<T extends any[], R> = T extends [infer A, ...infer Rest]
+  ? (arg: A) => CurryFunction<Rest, R>
+  : R;
+
+//currying function
+//example: sum(1,2) = sum(1)(2);
+  export const carrying = <T extends any[], R>(func: (...args: T) => R): CurryFunction<T, R> => {
+     return function curried(...args: any[]): any {
+         if (args.length >= func.length) {
+             return func(...args as T);
+         }
+
+         return function(...otherArgs: any[]) {
+             return curried(...([...args, ...otherArgs] as any[]));
+         }
+     } as CurryFunction<T, R>;
+ }
