7bc90f844ec93018e640b329cd48b97c0d8ca86f
Liubou Hadunova <liubou.hadunova@leverx.com>
2025-02-18 08:46:22 +0100

    feat: add partial utility

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,5 +1,10 @@
-export function partial<T extends (...args: any[]) => any>(func: T, ...funcArgs: Parameters<T>) {
-    return function (...otherArgs: Parameters<T>): ReturnType<T> {
+//type for partial function
+type PartialFunction = (...args: any[]) => any;
+
+//partial function
+//example: sum(1,2) => add1 = partial(sum, 1) => add1(2);
+export function partial<T extends PartialFunction, A extends any[]>(func: T, ...funcArgs: A) {
+    return function (...otherArgs: any[]) {
         return func(...funcArgs, ...otherArgs);
     };
 }
--- test/utils/partial.test.ts
+++ test/utils/partial.test.ts
@@ -2,12 +2,24 @@ import { describe, expect, test } from "@jest/globals";
 import { partial } from "../../src/utils/index";
 
 describe("partial application", () => {
-    test("adds 5 + 3 to equal 8", () => {
-        function sum(a: number, b: number): number {
-            return a + b;
-        }
-
-        const add5 = partial(sum, 5);
+    test("partial a function with 2 arguments", () => {
+        const add = (a: number, b: number) => a + b;
+      
+        const add5 = partial(add, 5);
         expect(add5(3)).toBe(8);
+    });
+
+    test("partial a function with 3 arguments", () => {
+        const multiple = (a: number, b: number, c: number) => a * b * c;
+      
+        const multipleBy2And5 = partial(multiple, 2, 5);
+        expect(multipleBy2And5(3)).toBe(30);
+    });
+
+    test("partial a function with no arguments", () => {
+        const hello = () => "Hello!";
+      
+        const result = partial(hello);
+        expect(result()).toBe("Hello!");
     });
 });
