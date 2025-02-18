ccd80d06a800ca1b57c2810398ab387d852d9e6d
Liubou Hadunova <liubou.hadunova@leverx.com>
2025-02-18 10:21:40 +0100

    feat: add filter utility

---
+++ test/utils/filter.test.ts
@@ -0,0 +1,29 @@
+import { describe, expect, test } from "@jest/globals";
+import { functionalFilter } from "../../src/utils/index";
+
+describe("functional filter function", () => {
+    test("filter an array with number values", () => {
+        const arr = [2, 5, 3, 1, 5];
+        const condition = (x: number) => x >= 3;
+      
+        const resultArr = functionalFilter(condition, arr);
+        console.log(resultArr);
+        expect(resultArr).toEqual([5, 3, 5]);
+    });
+
+    test("filter an empty array", () => {
+        const arr: number[] = [];
+        const condition = (x: number) => x >= 3;
+      
+        const resultArr = functionalFilter(condition, arr);
+        expect(resultArr).toEqual([]);
+    });
+
+    test("filter an array with string values", () => {
+        const arr = ["one", "two", "three", "four", "five", "six"];
+        const condition = (x: string) => x.length <= 3;
+      
+        const resultArr = functionalFilter(condition, arr);
+        expect(resultArr).toEqual(["one", "two", "six"]);
+    });
+});
