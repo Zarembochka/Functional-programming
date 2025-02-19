519a06dd743d437ce0d0875b06a9360478714e24
Liubou Hadunova <liubou.hadunova@leverx.com>
2025-02-19 10:24:07 +0100

    fix: update filter to work with array index

--- test/utils/filter.test.ts
+++ test/utils/filter.test.ts
@@ -25,4 +25,12 @@ describe("functional filter function", () => {
         const resultArr = functionalFilter(condition, arr);
         expect(resultArr).toEqual(["one", "two", "six"]);
     });
+
+    test("filter an array with index parameter", () => {
+        const arr = ["one", "two", "three", "four", "five", "six"];
+        const condition = (x: string, index: number) => index % 2 === 0
+      
+        const resultArr = functionalFilter(condition, arr);
+        expect(resultArr).toEqual(["one", "three", "five"]);
+    });
 });
